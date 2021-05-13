3c5397084bff4155e65597054517e1ce928a1ce4
kenjikenzel <80047778+kenjikenzel@users.noreply.github.com>
2021-05-13 12:45:14 +0200

    Add files via upload

--- Opdracht19_if_else_statements/index.js
+++ Opdracht19_if_else_statements/index.js
@@ -1,6 +1,29 @@
-console.log("Test");
 const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
 
-if (age > 18) {
-  document.write("ouder dan 18");
+// Testdata of statements werken
+
+/* 
+const age = 17;
+const isFemale = false;
+const driverStatus = "geenBob";
+*/
+
+if (age >= 18) {
+  console.log("Je mag naar binnen, je bent 18 jaar of ouder");
+} else {
+  console.log("Helaas, je mag niet naar binnen");
+}
+
+if (isFemale) {
+  console.log("Je bent een vrouw, je mag naar binnen");
+} else {
+  console.log("Je bent geen vrouw, je mag niet naar binnen");
+}
+
+if (driverStatus == "bob") {
+  console.log("Je bent de bob, je mag rijden!");
+} else {
+  console.log("Je bent geen bob, je mag niet autorijden!");
 }
