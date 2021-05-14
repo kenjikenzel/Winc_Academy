48e101f3f7fcd52c02966013a16de0df17d9e219
kenjikenzel <80047778+kenjikenzel@users.noreply.github.com>
2021-05-14 14:35:15 +0200

    Add files via upload

---
+++ Opdracht22_Function_basics/index.js
@@ -0,0 +1,20 @@
+//Defining the function
+const makePizza = function () {
+  console.log("Make the dough");
+  console.log("Decorate pizza");
+  console.log("Bake pizza");
+};
+
+const makeSushi = function () {
+  console.log("Cook sushi rice");
+  console.log("Prepare fish");
+  console.log("Roll sushi");
+};
+
+//Calling the function
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
