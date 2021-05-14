ee989b8f44df7116ecf66d6b416b14c0152ea21f
kenjikenzel <80047778+kenjikenzel@users.noreply.github.com>
2021-05-14 11:56:13 +0200

    Add files via upload

--- Opdracht20_comparison_logical_operators/index.js
+++ Opdracht20_comparison_logical_operators/index.js
@@ -1,8 +1,7 @@
 const age = 18;
 const isFemale = true;
 const driverStatus = "bob";
-const firstNameMale = "Bram";
-const firstNameFemale = "Sarah";
+const firstName = "Bram";
 const totalAmount = 25;
 
 // Testdata of statements werken
@@ -31,26 +30,24 @@ if (driverStatus == "bob") {
   console.log("Je bent geen bob, je mag niet autorijden!");
 }
 
-if (age > 18 && age < 25) {
+if (age >= 18 && age <= 25) {
   console.log("Je krijgt 50% korting!");
 } else {
   console.log("Je krijgt helaas geen 50% korting");
 }
 
-if (firstNameMale === "Bram" || firstNameFemale === "Sarah") {
+if (firstName === "Bram" || firstName === "Sarah") {
   console.log("Je krijgt een gratis biertje!");
 } else {
-  console.log("Je krijgt geen gratis biertje...");
+  console.log("Je heet geen Bram of Sarah, dus krijgt geen gratis biertje...");
 }
 
-if (totalAmount > 25 && totalAmount >= 50) {
-  console.log("Je krijgt gratis (vega)bitterballen");
-}
-
-if (totalAmount > 50 && totalAmount <= 100) {
-  console.log("Je krijgt gratis portie nachos");
-}
-
-if (totalAmount > 100) {
+if (totalAmount >= 100) {
   console.log("Je krijgt een gratis flesje champagne");
+} else if (totalAmount > 50) {
+  console.log("Je krijgt gratis portie nachos");
+} else if (totalAmount > 25) {
+  console.log("Je krijgt gratis (vega)bitterballen");
+} else {
+  console.log("Helaas je krijgt geen korting");
 }
