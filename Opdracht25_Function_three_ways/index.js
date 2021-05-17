d26c46456cb3b1c548c0664e0c9d5a87257f6e33
kenjikenzel <80047778+kenjikenzel@users.noreply.github.com>
2021-05-17 19:43:17 +0200

    Add files via upload

---
+++ Opdracht25_Function_three_ways/index.js
@@ -0,0 +1,8 @@
+function total(number1, number2) {
+  const squared = number1 * number2;
+  const addSquared = squared + squared;
+  const sumSquared = addSquared * addSquared;
+  return number1, number2;
+}
+
+console.log(total(2, 2));
