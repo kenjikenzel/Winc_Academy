bf7125727f04aca14de03821265c2deba4506a95
kenjikenzel <80047778+kenjikenzel@users.noreply.github.com>
2021-05-22 16:57:38 +0200

    Add files via upload

---
+++ Opdracht26_Calling_functions/index.js
@@ -0,0 +1,29 @@
+/* 
+// Hey Kiddo
+const ageCheck = function (age) {
+  if (age >= 18) {
+    return "TRUE";
+  } else {
+    return "FALSE";
+  }
+};
+
+console.log(ageCheck(17));
+console.log(ageCheck(18));
+
+const ageGreet = function () {
+  if ((ageCheck = "TRUE")) {
+    return "Hello there";
+  } else {
+    return "Hey kiddo";
+  }
+};
+
+ageGreet();
+
+// VAT calculations
+
+const VAT = function (price) {
+  return price * 1.21;
+};
+*/
