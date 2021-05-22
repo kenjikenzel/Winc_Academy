e65c1640b0df471d02f75899ae73a6bde04c05e6
kenjikenzel <80047778+kenjikenzel@users.noreply.github.com>
2021-05-22 20:37:35 +0200

    Add files via upload

--- Opdracht28_Iterators/index.js
+++ Opdracht28_Iterators/index.js
@@ -1,3 +1,18 @@
-let color = ["yellow", "blue", "red", "orange"];
+// Oude stijl: loops
+const colorArray = ["yellow", "blue", "red", "orange"];
 
-while (color);
+let i = 0;
+while (i < colorArray.length) {
+  console.log(colorArray[i]);
+  i++;
+}
+
+for (let i = 0; i < colorArray.length; i++) {
+  console.log(colorArray[i]);
+}
+
+// Nieuwe stijl: array methods
+
+const colorArray = ["yellow", "blue", "red", "orange"];
+
+colorArray.forEach((color) => console.log(color));
